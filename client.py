20ddac14f427e93f275fbf2acadcc9bf3ad86894
sertugkaya <sertugkaya@gmail.com>
2015-01-11 19:06:29 +0200

    threading & protocol messages

--- client.py
+++ client.py
@@ -3,6 +3,8 @@ import json
 __author__ = 'sertug'
 
 import socket
+import threading
+import time
 
 
 class Client:
@@ -10,6 +12,8 @@ class Client:
     __serverPort = 0
     __bufferSize = 0
     __clientsocket = None
+    __username = ""
+    __canheartbeat = False
 
     def __init__(self):
         self.__serverPort = 12345
@@ -17,10 +21,16 @@ class Client:
 
     def connect(self):
         self.__clientsocket = socket.socket()
-        #self.__serverAddress = socket.gethostname()
         self.__serverAddress = "localhost"
         self.__clientsocket.connect((self.__serverAddress, self.__serverPort))
         print("Connected....")
+
+    def heartbeat(self):
+        while self.__canheartbeat:
+            print "heartbeat sending"
+            data = {'code': 'CHBEAT', 'username': self.__username}
+            self.__clientsocket.send(json.dumps(data))
+            time.sleep(5)
 
     def disconnect(self):
         self.__clientsocket.close()
@@ -30,22 +40,52 @@ class Client:
         response = self.__clientsocket.recv(self.__bufferSize)
         print(response)
         response = json.loads(response)
-        print "unjson", (response)
-        print "header", response.get('header')
         return response
+
+class HeartBeatThread(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.__clientsocket = socket.socket()
+        self.__serverAddress = socket.gethostname()
+        self.__serverPort = 54321
+
+    def __del__(self):
+        self.__clientsocket.close()
+
+    def connect(self):
+        self.__clientsocket.connect((self.__serverAddress, self.__serverPort))
+        print("internal client connected....")
+
+    def send(self, data):
+        print "internal sending"
+        self.__clientsocket.send(data)
+
+    def run(self):
+        self.connect()
+        while True:
+            print "heartbeat sending"
+            data = {'code': 'CHBEAT', 'username': self.__username}
+            self.__clientsocket.send(json.dumps(data))
+            time.sleep(5)
 
 
 if __name__ == "__main__":
     clientObj = Client()
     clientObj.connect()
+    #threading.Timer(2.0, clientObj.heartbeat()).start()
 
     request = ""
     while request != "QUI":
         request = raw_input('>')
         userCode = request[0:6]
         userData = request[7:]
-        data = {'code': userCode, 'username': userData}
-        print json.dumps(data)
+        userDataList = userData.split()
+        print "userDataList ", userDataList
+        if userCode == "CCNREQ" or userCode == "CPLREQ" or userCode == "CGUREQ" or userCode == "CWRREQ" or userCode == "CLGREQ":
+            data = {'code': userCode, 'username': userDataList[0]}
+        elif userCode == "CMVREQ":
+            data = {'code': userCode, 'username': userDataList[0], 'move': userDataList[1]}
+        print "client sending: ", json.dumps(data)
         response = clientObj.sendrequest(json.dumps(data))
 
     clientObj.disconnect()
--- server.py
+++ server.py
@@ -3,31 +3,62 @@ __author__ = 'sertug'
 import socket
 import datetime
 import json
+import threading
 
-class Server:
+
+class ClientThread(threading.Thread):
+    """Client handling unit for server class"""
+    def __init__(self, clientsocket, clientaddress):
+        threading.Thread.__init__(self)
+        assert isinstance(clientsocket, socket.socket)
+        self.clientSocket = clientsocket
+        self.clientAddress = clientaddress
+
+    def __del__(self):
+        self.clientSocket.close()
+
+    def run(self):
+        while True:
+            request = self.clientSocket.recv(1024)
+            print "request arrived:" + request
+            response = self.parser(request)
+            self.clientSocket.send(json.dumps(response))
+
+    def parser(self, request):
+        request = json.loads(request)
+        if request is None:
+            data = {'header': 'SRVERR', 'message': 'error'}
+            response = json.dumps(data)
+        elif request.get('code') == "CCNREQ":
+            username = request.get('username')
+            response = {'header': 'SRVROK', 'username': username, 'message': 'Welcome to TavlaHero, please choose to be a player to play backgammon or choose to be a guest to watch a game live.'}
+        elif request.get('code') == "CHBEAT":
+            username = request.get('username')
+            response = {'header': 'SRVROK', 'username': username}
+        elif request.get('code') == "CPLREQ":
+            username = request.get('username')
+            response = {'header': 'SRVROK', 'username': username}
+        elif request.get('code') == "CGUREQ":
+            username = request.get('username')
+            response = {'header': 'SRVROK', 'username': username}
+        else:
+            data = {'header': 'SRVERR', 'message': 'error'}
+            response = json.dumps(data)
+        return response
+
+class Server(threading.Thread):
     __serverAddress = ""
     __serverPort = 0
     __serverSocket = None
     __definitions = {}
+    __exitFlag = 0
 
     def __init__(self):
+        threading.Thread.__init__(self)
         self.__serverPort = 12345
         self.__bufferSize = 1024
 
-    def __loaddefinitions(self):
-        f = file("codes.txt", "r")
-        for line in f:
-            line = line.strip()
-            splitresult = line.split(' ', 1)
-            if splitresult.__len__() == 2:
-                self.__definitions[splitresult[1]] = splitresult[0]
-            else:
-                raise Exception("Error in codes file!")
-        f.close()
-
     def start(self):
-        self.__loaddefinitions()
-
         self.__serverSocket = socket.socket()
         # self.__serverAddress = socket.gethostname()
         self.__serverAddress = "localhost"
@@ -35,53 +66,14 @@ class Server:
         self.__serverSocket.listen(5)
         print("Server is listening on ip: " + self.__serverAddress)
         print("Server is listening on port: " + str(self.__serverPort))
-
-        clientsocket, clientaddress = self.__serverSocket.accept()
-        print "Client connected from: ", clientaddress
-        while True:
-            request = clientsocket.recv(self.__bufferSize)
-            print "request arrived:" + request
-            response = self.parser(request)
-            clientsocket.send(json.dumps(response))
-            print "Data sent:" + response
-            if response == "BYE":
-                break
+        while not Server.__exitFlag:
+            clientsocket, clientaddress = self.__serverSocket.accept()
+            if not Server.__exitFlag:
+                client_thread = ClientThread(clientsocket, clientaddress)
+                client_thread.start()
 
         clientsocket.close()
         self.__serverSocket.close()
-
-    def parser(self, request):
-        request = json.loads(request)
-
-        if request is None:
-            data = {'header': 'SRVERR', 'message': 'error'}
-            response = json.dumps(data)
-        elif request.get('code') == "CCNREQ":
-            username = request.get('username')
-            print "got username", username
-            response = {'header': 'SRVROK', 'message': 'welcome'}
-        elif strippedrequest[0:6] == "CHBEAT":
-            username = strippedrequest[7:]
-            response = username
-        elif strippedrequest[0:3] == "CPLREQ":
-            username = strippedrequest[7:]
-            response = "TOC " + datetime.datetime.now().strftime("%d/%m/%Y, %H:%M")
-        elif strippedrequest[0:6] == "CGUREQ":
-            username = strippedrequest[7:]
-            response = username
-        elif strippedrequest[0:3] == "GET":
-            key = strippedrequest[4:]
-            if key in self.__definitions:
-                response = "CDE " + self.__definitions[key]
-            else:
-                response = "NTF " + key
-        elif strippedrequest[0:3] == "QUI":
-            response = "BYE"
-        else:
-            data = {'header': 'SRVERR', 'message': 'error'}
-            response = json.dumps(data)
-
-        return response
 
 
 if __name__ == "__main__":
